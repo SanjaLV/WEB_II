efc86c285231c20aee02bb12749f3842f9031938
SanjaLV <GhostSanja@gmail.com>
2018-06-16 01:59:34 +0300

    day 1 end

--- main/models.py
+++ main/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Item(models.Model):
+    item_level = models.IntegerField(default=0)
+    item_tupe = models.IntegerField()
+    item_rarity = models.IntegerField()
+
+
+class Affix(models.Model):
+    affix_tupe = models.IntegerField()
+    affix_value = models.IntegerField()
+    item_id = models.ForeignKey(Item, on_delete=models.CASCADE)
+
+
+
