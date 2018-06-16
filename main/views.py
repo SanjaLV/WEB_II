1c4c0b6fe0c35b2559d0d7c89140a062ed72dfc7
SanjaLV <GhostSanja@gmail.com>
2018-06-16 13:53:56 +0300

    Starting to work on users

--- main/views.py
+++ main/views.py
@@ -4,4 +4,4 @@ from django.utils.translation import gettext_lazy as _
 
 # Create your views here.
 def index(request):
-    return HttpResponse(_("Hello Here!"))
+    return render(request, 'base.html')
